70ccd4230cc9344467d461712803b218f8ffe5c6
Marjoram Iliana <marjorai@ox-s2.msk.21-school.ru>
2026-06-13 19:52:15 +0300

    V

--- go_server/logger/logger.go
+++ go_server/logger/logger.go
@@ -1,3 +1,72 @@
-package logger
+package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
 
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const LogDirPath = "logs"
+
+type Logger struct {
+	logger *zap.Logger
+}
+
+func createFile(dirPath, filename string) (*os.File, error) {
+	if err := os.MkdirAll(dirPath, 0755); err != nil {
+		return nil, fmt.Errorf("failed to create log directory: %w", err)
+	}
+
+	fullPath := filepath.Join(dirPath, filename)
+
+	file, err := os.OpenFile(fullPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open log file: %w", err)
+	}
+
+	return file, nil
+}
+
+func NewLogger(filename string) *Logger {
+	file, err := createFile(LogDirPath, filename)
+	if err != nil {
+		logger, _ := zap.NewProduction()
+		return &Logger{logger: logger}
+	}
+
+	config := zap.NewProductionConfig()
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	fileEncoder := zapcore.NewJSONEncoder(config.EncoderConfig)
+	writer := zapcore.Lock(file)
+	defaultLogLevel := zapcore.DebugLevel
+
+	core := zapcore.NewTee(
+		zapcore.NewCore(fileEncoder, writer, defaultLogLevel),
+	)
+
+	logger := zap.New(core, zap.AddStacktrace(zapcore.ErrorLevel), zap.AddCaller())
+
+	return &Logger{
+		logger: logger,
+	}
+}
+
+func (l *Logger) Info(msg string, fields ...zap.Field) {
+	l.logger.Info(msg, fields...)
+}
+
+func (l *Logger) Error(msg string, fields ...zap.Field) {
+	l.logger.Error(msg, fields...)
+}
+
+func (l *Logger) Fatal(msg string, fields ...zap.Field) {
+	l.logger.Fatal(msg, fields...)
+}
+
+func (l *Logger) Debug(msg string, fields ...zap.Field) {
+	l.logger.Debug(msg, fields...)
+}
